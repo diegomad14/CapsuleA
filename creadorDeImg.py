a17d0cc6c32b0dd78f9dd02209f39952a7b13db4
diegomad14 <diego.andred@hotmail.com>
2022-01-17 20:00:03 -0500

    V1.1

--- creadorDeImg.py
+++ creadorDeImg.py
@@ -8,8 +8,8 @@ lunes_1 = os.path.join(FONDOS_LUNES,'Lunes-Jueves.png')
 martes_1 = os.path.join(FONDOS_MARTES,'Martes-Viernes.png')
 miercoles_1 = os.path.join(FONDOS_MIERCOLES,'Miercoles-Sabado.png')
 
-dirfntB = os.path.join(FONTS_DIR,'Bebas Neue Pro Bold.otf')
-dirfntR = os.path.join(FONTS_DIR,'Bebas Neue Pro Regular.otf')
+dirfntB = os.path.join(FONTS_DIR,'Bison-Bold(PersonalUse).ttf')
+dirfntR = os.path.join(FONTS_DIR,'Bison-DemiBold.ttf')
 
 
 def imagen_1(dia, titulo, numeroC):
@@ -26,7 +26,7 @@ def imagen_1(dia, titulo, numeroC):
 
     # get a font
     fntB = ImageFont.truetype(dirfntB, 161)
-    fntR = ImageFont.truetype(dirfntR, 40)
+    fntR = ImageFont.truetype(dirfntR, 44)
     # get a drawing context
     d = ImageDraw.Draw(txt)
 
@@ -34,14 +34,8 @@ def imagen_1(dia, titulo, numeroC):
     numeroPalabras = 0
     diaC = dia
     separado = titulo.split()
-    purple = (241,90,36,255)
     white = (255,255,255,255)
-    color = ""
-
-    if dia == "Miercoles-Sábado":
-        color = white
-    else:
-        color = "#4c4c4c"
+    color = white
 
     for i in separado:
         numeroPalabras += 1
@@ -57,82 +51,86 @@ def imagen_1(dia, titulo, numeroC):
     if numeroPalabras == 2:
 
         
-        d.text((165.5078/1.3,394.4723/1.32), separado[0], font=fntB, fill=color)
+        d.text((165.5078/1.3,394.4723/1.56), separado[0], font=fntB, fill=color)
         d.text((165.5078/1.3,394.4723), separado[1], font=fntB, fill=color)
     if numeroPalabras == 3:
 
         
-        d.text((165.5078/1.3,394.4723/1.59), separado[0]+" "+separado[1], font=fntB, fill=color)
+        d.text((165.5078/1.3,394.4723/1.56), separado[0]+" "+separado[1], font=fntB, fill=color)
         d.text((165.5078/1.3,394.4723), separado[2], font=fntB, fill=color)
 
     if numeroPalabras == 4:
 
         
-        d.text((165.5078/1.3,394.4723/1.37), separado[0]+" "+separado[1], font=fntB, fill=color)
+        d.text((165.5078/1.3,394.4723/1.56), separado[0]+" "+separado[1], font=fntB, fill=color)
         d.text((165.5078/1.3,394.4723), separado[2]+" "+separado[3], font=fntB, fill=color)
 
     if numeroPalabras == 5:
-        fntB = ImageFont.truetype(dirfntB, 111)
-        if len(separado[0])+len(separado[1])+len(separado[2]) <= 16:
-            d.text((165.5078/1.3,394.4723/1.3), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723),separado[3]+" "+separado[4], font=fntB, fill=color)
-        else: 
-            d.text((165.5078/1.3,394.4723/1.3), separado[0]+" "+separado[1], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723),separado[2]+" "+separado[3]+" "+separado[4], font=fntB, fill=color)
+        fntB = ImageFont.truetype(dirfntB, 161)
+        #if len(separado[0])+len(separado[1])+len(separado[2]) <= 16:
+        d.text((165.5078/1.3,394.4723/1.56), separado[0]+" "+separado[1], font=fntB, fill=color)
+        d.text((165.5078/1.3,394.4723),separado[2]+" "+separado[3], font=fntB, fill=color)
+        d.text((165.5078/1.3,394.4723*1.365),separado[4], font=fntB, fill=color)
+        #else:
+        #d.text((165.5078/1.3,394.4723/1.56), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
+        #d.text((165.5078/1.3,394.4723),separado[3]+" "+separado[4], font=fntB, fill=color)
 
 
     if numeroPalabras == 6:
-        fntB = ImageFont.truetype(dirfntB, 91)
+        fntB = ImageFont.truetype(dirfntB, 157)
         if len(separado[0])+len(separado[1])+len(separado[2]) <= 16:
-            d.text((165.5078/1.3,394.4723/1.3), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723),separado[3]+" "+separado[4]+" "+separado[5], font=fntB, fill=color)
-        else: 
-            d.text((165.5078/1.3,394.4723/1.3), separado[0]+" "+separado[1], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723),separado[2]+" "+separado[3]+" "+separado[4]+" "+separado[5], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723/1.7387), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723/1.085),separado[3]+" "+separado[4]+" "+separado[5], font=fntB, fill=color)
+        #    d.text((165.5078/1.3,394.4723*1.27),separado[5], font=fntB, fill=color)
+        #else: 
+        #    d.text((165.5078/1.3,394.4723/1.56), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
+        #    d.text((165.5078/1.3,394.4723/1),separado[3]+" "+separado[4], font=fntB, fill=color)
+        #    d.text((165.5078/1.3,394.4723*1.27),separado[5], font=fntB, fill=color)
 
 
     if numeroPalabras == 7:
-        fntB = ImageFont.truetype(dirfntB, 86)
+        fntB = ImageFont.truetype(dirfntB, 157)
         if len(separado[0])+len(separado[1])+len(separado[2]) <= 18:
-            d.text((165.5078/1.3,394.4723/1.38), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723/1.7387), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
             d.text((165.5078/1.3,394.4723/1.085),separado[3]+" "+separado[4], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723*1.12),separado[5] +" "+separado[6], font=fntB, fill=color)
-        else: 
-            d.text((165.5078/1.3,394.4723/1.3), separado[0]+" "+separado[1], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723),separado[2]+" "+separado[3]+" "+separado[4]+" "+separado[5], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723),separado[3]+" "+separado[4]+" "+separado[5], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723*1.27),separado[5] +" "+separado[6], font=fntB, fill=color)
+        #else: 
+        #    d.text((165.5078/1.3,394.4723/1.3), separado[0]+" "+separado[1], font=fntB, fill=color)
+        #    d.text((165.5078/1.3,394.4723),separado[2]+" "+separado[3]+" "+separado[4]+" "+separado[5], font=fntB, fill=color)
+        #    d.text((165.5078/1.3,394.4723),separado[3]+" "+separado[4]+" "+separado[5], font=fntB, fill=color)
 
     if numeroPalabras == 8:
-        fntB = ImageFont.truetype(dirfntB, 86)
+        fntB = ImageFont.truetype(dirfntB, 157)
         if len(separado[0])+len(separado[1])+len(separado[2]) <= 18:
-            d.text((165.5078/1.3,394.4723/1.38), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723/1.7387), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
             d.text((165.5078/1.3,394.4723/1.085),separado[3]+" "+separado[4], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723*1.12),separado[5] +" "+separado[6]+" "+separado[7], font=fntB, fill=color)
-        else: 
-            d.text((165.5078/1.3,394.4723/1.38), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723/1.085),separado[3]+" "+separado[4], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723*1.12),separado[5] +" "+separado[6]+" "+separado[7], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723*1.27),separado[5] +" "+separado[6], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723*1.6173),separado[7], font=fntB, fill=color)
+        #else: 
+        #    d.text((165.5078/1.3,394.4723/1.7387), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
+        #    d.text((165.5078/1.3,394.4723/1.085),separado[3]+" "+separado[4], font=fntB, fill=color)
+        #    d.text((165.5078/1.3,394.4723*1.27),separado[5] +" "+separado[6]+" "+separado[7], font=fntB, fill=color)
 
     if numeroPalabras == 9:
-        fntB = ImageFont.truetype(dirfntB, 86)
+        fntB = ImageFont.truetype(dirfntB, 157)
         if len(separado[0])+len(separado[1])+len(separado[2]) <= 18:
-            d.text((165.5078/1.3,394.4723/1.38), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723/1.7387), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
             d.text((165.5078/1.3,394.4723/1.085),separado[3]+" "+separado[4]+" "+separado[5], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723*1.12),separado[6]+" "+separado[7]+" "+separado[8], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723*1.27),separado[6]+" "+separado[7]+" "+separado[8], font=fntB, fill=color)
 
     if numeroPalabras == 10:
-        fntB = ImageFont.truetype(dirfntB, 86)
+        fntB = ImageFont.truetype(dirfntB, 157)
         if len(separado[0])+len(separado[1])+len(separado[2]) <= 18:
-            d.text((165.5078/1.3,394.4723/1.38), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723/1.7387), separado[0]+" "+separado[1]+" "+separado[2], font=fntB, fill=color)
             d.text((165.5078/1.3,394.4723/1.085),separado[3]+" "+separado[4]+" "+separado[5], font=fntB, fill=color)
-            d.text((165.5078/1.3,394.4723*1.12),separado[6]+" "+separado[7]+" "+separado[8]+" "+separado[9], font=fntB, fill=color)
+            d.text((165.5078/1.3,394.4723*1.27),separado[6]+" "+separado[7]+" "+separado[8]+" "+separado[9], font=fntB, fill=color)
 
     if diaC == "Lunes-Jueves":
-            d.text((165.5078*1.019/1.3,394.4723*1.48),numeroC, font=fntR, fill=(76, 76, 76,255))
+        d.text((165.5078*2.45,394.4723*2.123),numeroC, font=fntR, fill=(76, 76, 76,255))
     if diaC == "Martes-Viernes":
-        d.text((165.5078*1.019/1.3,394.4723*1.48),numeroC, font=fntR, fill=(76, 76, 76,255))
+        d.text((165.5078*2.5,394.4723*2.123),numeroC, font=fntR, fill=(105, 57, 23,255))
     if diaC == "Miercoles-Sábado":
-        d.text((165.5078*1.019/1.3,394.4723*1.48),numeroC, font=fntR, fill=(128,0,128, 255))
+        d.text((165.5078*2.59,394.4723*2.123),numeroC, font=fntR, fill=(115, 45, 145, 255))
 
 
     out = Image.alpha_composite(base, txt)
@@ -140,5 +138,3 @@ def imagen_1(dia, titulo, numeroC):
     out.show()
 
     return out
-
-imagen_1("Lunes-Jueves","MI AMIGO JESÚS","10")
